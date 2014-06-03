acf6b3a208a1681b0bad377c65c66aef94fe9f98
Lai Jiangshan <laijs@cn.fujitsu.com>
2014-06-03 15:32:07 +0800

    Revert "sock_diag: define destruction multicast groups"
    This reverts commit 6029378a82c6343e3db7fdfbcd79c59149959d85.

--- net/core/sock.c
+++ net/core/sock.c
@@ -1384,7 +1384,7 @@ struct sock *sk_alloc(struct net *net, int family, gfp_t priority,
 }
 EXPORT_SYMBOL(sk_alloc);
 
-void sk_destruct(struct sock *sk)
+static void __sk_free(struct sock *sk)
 {
 	struct sk_filter *filter;
 
@@ -1414,14 +1414,6 @@ void sk_destruct(struct sock *sk)
 	put_pid(sk->sk_peer_pid);
 	put_net(sock_net(sk));
 	sk_prot_free(sk->sk_prot_creator, sk);
-}
-
-static void __sk_free(struct sock *sk)
-{
-	if (unlikely(sock_diag_has_destroy_listeners(sk)))
-		sock_diag_broadcast_destroy(sk);
-	else
-		sk_destruct(sk);
 }
 
 void sk_free(struct sock *sk)

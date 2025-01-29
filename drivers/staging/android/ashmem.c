3c68cb984c692e0329aab475e5ef75f49adcea4e
djokcho <probgdjokcho@gmail.com>
2025-01-29 15:23:27 +0200

    Revert "staging: android: ashmem: Fix mmap size validation"
    This reverts commit f53dc5e363188f1f9a6a8622f2cabbb678d5215b.

--- drivers/staging/android/ashmem.c
+++ drivers/staging/android/ashmem.c
@@ -312,12 +312,6 @@ static int ashmem_mmap(struct file *file, struct vm_area_struct *vma)
 		ret = -EINVAL;
 	}
 
-	/* requested mapping size larger than object size */
-	if (vma->vm_end - vma->vm_start > PAGE_ALIGN(asma->size)) {
-		ret = -EINVAL;
-		goto out;
-	}
-
 	/* requested protection bits must match our allowed protection mask */
 	if (unlikely((vma->vm_flags & ~calc_vm_prot_bits(prot_mask)) &
 		     calc_vm_prot_bits(PROT_MASK)))

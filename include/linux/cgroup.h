71829bb826f5ac1852dacd0deffbd4d8f7d14cdd
djokcho <probgdjokcho@gmail.com>
2025-02-28 19:51:10 +0200

    Revert "cgroup: convert CFTYPE_* flags to enums"
    This reverts commit 5d1c0290b7446f4c41c17b6eba9b04bf7f061db4.

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -405,11 +405,9 @@ struct cgroup_map_cb {
  */
 
 /* cftype->flags */
-enum {
-	CFTYPE_ONLY_ON_ROOT	= (1 << 0),	/* only create on root cg */
-	CFTYPE_NOT_ON_ROOT	= (1 << 1),	/* don't create on root cg */
-	CFTYPE_INSANE		= (1 << 2),	/* don't create if sane_behavior */
-};
+#define CFTYPE_ONLY_ON_ROOT	(1U << 0)	/* only create on root cg */
+#define CFTYPE_NOT_ON_ROOT	(1U << 1)	/* don't create on root cg */
+#define CFTYPE_INSANE		(1U << 2)	/* don't create if sane_behavior */
 
 #define MAX_CFTYPE_NAME		64
 

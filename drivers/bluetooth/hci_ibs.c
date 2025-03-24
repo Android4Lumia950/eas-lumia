1d993a9c4776274e80ae214a5d648119b455c900
djokcho <probgdjokcho@gmail.com>
2025-03-24 13:45:35 +0200

    Update bluetooth from KingKernel

--- drivers/bluetooth/hci_ibs.c
+++ drivers/bluetooth/hci_ibs.c
@@ -21,11 +21,6 @@
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
  */
 
 #include <linux/module.h>
@@ -100,7 +95,7 @@ static unsigned long tx_idle_delay = (HZ * 2);
 
 struct hci_ibs_cmd {
 	u8 cmd;
-} __attribute__((packed));
+} __packed;
 
 struct ibs_struct {
 	unsigned long rx_state;
@@ -231,7 +226,7 @@ static int send_hci_ibs_cmd(u8 cmd, struct hci_uart *hu)
 	struct ibs_struct *ibs = hu->priv;
 	struct hci_ibs_cmd *hci_ibs_packet;
 
-	BT_DBG("hu %p cmd 0x%x", hu, cmd);
+	BT_DBG("hu %pK cmd 0x%x", hu, cmd);
 
 	/* allocate packet */
 	skb = bt_skb_alloc(1, GFP_ATOMIC);
@@ -259,7 +254,7 @@ static void ibs_wq_awake_device(struct work_struct *work)
 	struct hci_uart *hu = (struct hci_uart *)ibs->ibs_hu;
 	unsigned long flags;
 
-	BT_DBG(" %p ", hu);
+	BT_DBG(" %pK ", hu);
 
 	/* Vote for serial clock */
 	ibs_msm_serial_clock_vote(HCI_IBS_TX_VOTE_CLOCK_ON, hu);
@@ -286,7 +281,7 @@ static void ibs_wq_awake_rx(struct work_struct *work)
 	struct hci_uart *hu = (struct hci_uart *)ibs->ibs_hu;
 	unsigned long flags;
 
-	BT_DBG(" %p ", hu);
+	BT_DBG(" %pK ", hu);
 
 	ibs_msm_serial_clock_vote(HCI_IBS_RX_VOTE_CLOCK_ON, hu);
 
@@ -314,7 +309,7 @@ static void ibs_wq_serial_rx_clock_vote_off(struct work_struct *work)
 					ws_rx_vote_off);
 	struct hci_uart *hu = (struct hci_uart *)ibs->ibs_hu;
 
-	BT_DBG(" %p ", hu);
+	BT_DBG(" %pK ", hu);
 
 	ibs_msm_serial_clock_vote(HCI_IBS_RX_VOTE_CLOCK_OFF, hu);
 
@@ -326,7 +321,7 @@ static void ibs_wq_serial_tx_clock_vote_off(struct work_struct *work)
 					ws_tx_vote_off);
 	struct hci_uart *hu = (struct hci_uart *)ibs->ibs_hu;
 
-	BT_DBG(" %p ", hu);
+	BT_DBG(" %pK ", hu);
 
 	hci_uart_tx_wakeup(hu);  /* run HCI tx handling unlocked */
 
@@ -342,7 +337,7 @@ static void hci_ibs_tx_idle_timeout(unsigned long arg)
 	struct ibs_struct *ibs = hu->priv;
 	unsigned long flags;
 
-	BT_DBG("hu %p idle timeout in %lu state", hu, ibs->tx_ibs_state);
+	BT_DBG("hu %pK idle timeout in %lu state", hu, ibs->tx_ibs_state);
 
 	spin_lock_irqsave_nested(&ibs->hci_ibs_lock,
 					flags, SINGLE_DEPTH_NESTING);
@@ -376,7 +371,7 @@ static void hci_ibs_wake_retrans_timeout(unsigned long arg)
 	unsigned long flags;
 	unsigned long retransmit = 0;
 
-	BT_DBG("hu %p wake retransmit timeout in %lu state",
+	BT_DBG("hu %pK wake retransmit timeout in %lu state",
 		hu, ibs->tx_ibs_state);
 
 	spin_lock_irqsave_nested(&ibs->hci_ibs_lock,
@@ -409,7 +404,7 @@ static int ibs_open(struct hci_uart *hu)
 {
 	struct ibs_struct *ibs;
 
-	BT_DBG("hu %p", hu);
+	BT_DBG("hu %pK", hu);
 
 	ibs = kzalloc(sizeof(*ibs), GFP_ATOMIC);
 	if (!ibs)
@@ -505,7 +500,7 @@ static int ibs_flush(struct hci_uart *hu)
 {
 	struct ibs_struct *ibs = hu->priv;
 
-	BT_DBG("hu %p", hu);
+	BT_DBG("hu %pK", hu);
 
 	skb_queue_purge(&ibs->tx_wait_q);
 	skb_queue_purge(&ibs->txq);
@@ -518,22 +513,22 @@ static int ibs_close(struct hci_uart *hu)
 {
 	struct ibs_struct *ibs = hu->priv;
 
-	BT_DBG("hu %p", hu);
+	BT_DBG("hu %pK", hu);
 
 	skb_queue_purge(&ibs->tx_wait_q);
 	skb_queue_purge(&ibs->txq);
-	del_timer(&ibs->tx_idle_timer);
-	del_timer(&ibs->wake_retrans_timer);
+
+	del_timer_sync(&ibs->tx_idle_timer);
+	destroy_workqueue(ibs->workqueue);
+	del_timer_sync(&ibs->wake_retrans_timer);
+
 	__ibs_msm_serial_clock_request_off(hu->tty);
 	ibs_msm_serial_clock_vote(HCI_IBS_VOTE_STATS_UPDATE, hu);
 	ibs_log_local_stats(ibs);
-	destroy_workqueue(ibs->workqueue);
+
 	ibs->ibs_hu = NULL;
-
 	kfree_skb(ibs->rx_skb);
-
 	hu->priv = NULL;
-
 	kfree(ibs);
 
 	return 0;
@@ -547,7 +542,7 @@ static void ibs_device_want_to_wakeup(struct hci_uart *hu)
 	unsigned long flags;
 	struct ibs_struct *ibs = hu->priv;
 
-	BT_DBG("hu %p", hu);
+	BT_DBG("hu %pK", hu);
 
 	/* lock hci_ibs state */
 	spin_lock_irqsave(&ibs->hci_ibs_lock, flags);
@@ -596,7 +591,7 @@ static void ibs_device_want_to_sleep(struct hci_uart *hu)
 	unsigned long flags;
 	struct ibs_struct *ibs = hu->priv;
 
-	BT_DBG("hu %p", hu);
+	BT_DBG("hu %pK", hu);
 
 	/* lock hci_ibs state */
 	spin_lock_irqsave(&ibs->hci_ibs_lock, flags);
@@ -624,7 +619,7 @@ static void ibs_device_want_to_sleep(struct hci_uart *hu)
 }
 
 /*
- * Called upon wake-up-acknowledgement from the device
+ * Called upon wake-up-acknowledgment from the device
  */
 static void ibs_device_woke_up(struct hci_uart *hu)
 {
@@ -632,7 +627,7 @@ static void ibs_device_woke_up(struct hci_uart *hu)
 	struct ibs_struct *ibs = hu->priv;
 	struct sk_buff *skb = NULL;
 
-	BT_DBG("hu %p", hu);
+	BT_DBG("hu %pK", hu);
 
 	/* lock hci_ibs state */
 	spin_lock_irqsave(&ibs->hci_ibs_lock, flags);
@@ -677,7 +672,7 @@ static int ibs_enqueue(struct hci_uart *hu, struct sk_buff *skb)
 	unsigned long flags = 0;
 	struct ibs_struct *ibs = hu->priv;
 
-	BT_DBG("hu %p skb %p", hu, skb);
+	BT_DBG("hu %pK skb %pK", hu, skb);
 
 	/* Prepend skb with frame type */
 	memcpy(skb_push(skb, 1), &bt_cb(skb)->pkt_type, 1);
@@ -757,7 +752,7 @@ static int ibs_recv(struct hci_uart *hu, void *data, int count)
 	struct hci_sco_hdr   *sh;
 	register int len, type, dlen;
 
-	BT_DBG("hu %p count %d rx_state %ld rx_count %ld",
+	BT_DBG("hu %pK count %d rx_state %ld rx_count %ld",
 			hu, count, ibs->rx_state, ibs->rx_count);
 
 	ptr = data;
@@ -877,6 +872,7 @@ static int ibs_recv(struct hci_uart *hu, void *data, int count)
 static struct sk_buff *ibs_dequeue(struct hci_uart *hu)
 {
 	struct ibs_struct *ibs = hu->priv;
+
 	return skb_dequeue(&ibs->txq);
 }
 
